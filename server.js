50a57cc31f4f548c7913e8b0f867be7cec02d8f8
Thorben Bender <bender.thorben0@gmail.com>
2019-03-28 16:45:45 +0100

    added api for getting the dishes

--- server.js
+++ server.js
@@ -6,4 +6,16 @@ const db = knex(knexConfig);
 
 const server = express();
 
+server.get('/api/dishes', async (req, res) => {
+	try {
+		dishes = await db.raw('select * from dishes');
+		if (dishes) {
+			res.status(200).json(dishes);
+		}
+	} catch (error) {
+		console.log(error);
+		res.status(500).json({ message: 'Error getting the data from the server' });
+	}
+});
+
 module.exports = server;
